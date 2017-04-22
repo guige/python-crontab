6153f390b4b3cb603b685147b5825f9bdc61432b
Martin Owens <doctormo@gmail.com>
2017-04-22 17:42:15 -0400

    Use system wide coverage modules instead (not sure about this)

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,6 @@
 
-coverage3 run --source crontab,crontabs,cronlog setup.py test &> /dev/null
-coverage2 run -a --source crontab,crontabs,cronlog setup.py test &> /dev/null
-coverage3 report -m
+python3-coverage run --source crontab,crontabs,cronlog setup.py test &> /dev/null
+python2.6-coverage run -a --source crontab,crontabs,cronlog setup.py test &> /dev/null
+python2.7-coverage run -a --source crontab,crontabs,cronlog setup.py test &> /dev/null
+python3-coverage report -m
 
